15641fab266046161828473de28eef1f0792ccba
gtyuki83 <yukiukiuki0803@gmail.com>
2022-08-20 12:56:19 +0900

    edited profile

--- src/presentation/view_interfaces/MyPage.tsx
+++ src/presentation/view_interfaces/MyPage.tsx
@@ -5,10 +5,25 @@ import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Avatar from '@material-ui/core/Avatar';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+
+import Grid from "@material-ui/core/Grid";
+
+import { AccountCircle, AccessAlarm, ThreeDRotation, AccountBalanceWallet } from '@material-ui/icons';
 
 const useStyles = makeStyles({
   root: {
     flexGrow: 1,
+  },
+  grid: {
+    width: '100%',
+    maxWidth: 360,
+    bgcolor: 'background.paper',
   },
 });
 
@@ -36,10 +51,10 @@ const TabPanel = (props) => {
 const MyPage = (props) => {
   const hoge: string = "";
 
-  const CenteredTabs = (props) => {
-    const classes = useStyles();
-    const [value, setValue] = React.useState(0);
+  const classes = useStyles();
 
+  const CenteredTabs = (props) => {
+    const [value, setValue] = React.useState(0);
     const handleChange = (event, newValue) => {
       setValue(newValue);
     };
@@ -54,7 +69,7 @@ const MyPage = (props) => {
             textColor="primary"
             centered
           >
-            {props.labels.map(label => <Tab label={label}></Tab>)} {/* さっきの */}
+            {props.labels.map(label => <Tab label={label}></Tab>)}
           </Tabs>
         </Paper>
 
@@ -68,7 +83,44 @@ const MyPage = (props) => {
 
 
 
-  return <div>
+  return <div className={classes.root}>
+    <Avatar
+      alt="Iron Man"
+      src="https://assets.stickpng.com/images/580b57fbd9996e24bc43c051.png"
+    // sx={{ width: 56, height: 56 }}
+    />
+    <Grid container>
+      <Grid item>
+        <List component="nav" aria-label="mailbox folders">
+          <ListItem button>
+            <ListItemIcon>
+              <AccountCircle />
+            </ListItemIcon>
+            <ListItemText primary="Name" />
+          </ListItem>
+          <Divider />
+          <ListItem>
+            <ListItemText primary="0xUYZ" />
+          </ListItem>
+          <Divider />
+        </List>
+      </Grid>
+      <Grid item>
+        <List component="nav" aria-label="mailbox folders">
+          <ListItem button>
+            <ListItemIcon>
+              <AccountBalanceWallet />
+            </ListItemIcon>
+            <ListItemText primary="Address" />
+          </ListItem>
+          <Divider />
+          <ListItem>
+            <ListItemText primary="0x3a0bE810754f7f7D04fCA10E2C11E93ebb5BF19e" />
+          </ListItem>
+          <Divider />
+        </List>
+      </Grid>
+    </Grid>
     <CenteredTabs labels={['label1', 'label2', 'label3']}>
       <div>aaaaaaa
         <br />
@@ -77,7 +129,7 @@ const MyPage = (props) => {
       <div>bb</div>
       <div>cc</div>
     </CenteredTabs>
-  </div>;
+  </div >;
 };
 
 export default MyPage;
